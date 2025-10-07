7f7b83ada8b2dd384c6dfedf89125d9afb938807
kazemono <sanya016cool@gmail.com>
2025-10-07 21:33:41 +0500

    Calculation

---
+++ Новая папка/main.py
@@ -0,0 +1,12 @@
+n1 = int(input())
+operation = input()
+n2 = int(input())
+
+if operation == "+":
+    print(n1 + n2)
+elif operation == "-":
+    print(n1 - n2)
+elif operation == "*":
+    print(n1 * n2)
+else:
+    print(n1 / n2)
